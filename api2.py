3d334bc2678abdaf9df574fd5cd679deb710d74e
Kappa Carrageenan <proneo25@gmail.com>
2018-11-26 23:40:26 +0100

    new api with comprehensive comments - PEP8 style

--- api2.py
+++ api2.py
@@ -1,21 +1,19 @@
-# meine fake RPi import. ersetze durch den auskommentierten, damit der code auf windows läuft
 # import für die sleep funktionen
 import time
 # import fürs multithreading (die dummy library erlaubt umgang mit threads)
 from multiprocessing.dummy import Pool
 
+# meine fake RPi import. ersetze durch den auskommentierten, damit der code auf windows läuft
 import FakeRPi.GPIO as IO
+# import RPi.GPIO as IO
 # numpy import für die arrays
 import numpy as np
-
-# import RPi.GPIO as IO
-
 
 ###############
 
 # INHALT
 # 01: attribute der api (led array, pin arrays etc)
-# 02: softwareseitige funktionen (ledOn, ledOff) mit variablen parameterlängen
+# 02: softwareseitige funktionen (led_on, led_off) mit variablen parameterlängen
 # 03: hardwareseitige funktionen (hilfsfunktionen fürs programmverhalten)
 # 04: eigentlicher api code mit threading und speisen der schieberegister
 
@@ -43,66 +41,66 @@ leds = np.array([[[0 for x in range(cubeSize)] for y in range(cubeSize)] for z i
 # 02: SOFTWARESEITIGE FUNKTIONEN
 
 # schaltet beliebige menge an led's an
-def ledOn(*targetLeds):
-    for x in targetLeds:
+def led_on(*target_leds):
+    for x in target_leds:
         leds[x[0]][x[1]][x[2]] = 1
 
 
 # schaltet beliebige menge an led's aus
-def ledOff(*targetLeds):
-    for x in targetLeds:
+def led_off(*target_leds):
+    for x in target_leds:
         leds[x[0]][x[1]][x[2]] = 0
 
 
 # 03: HARDWARESEITIGE FUNKTIONEN
 
 # setup der pins
-def ledSetup():
+def led_setup():
     IO.setmode(IO.BCM)
     for x in np.ravel(anodePins):
         IO.setup(x, IO.OUT)
 
 
 # erzeugt 16 bit information für die shift register paare als array
-def getSet(x, y):
+def get_set(x, y):
     return np.ravel(leds[x, (y * 2):(y * 2 + 2), 0:])
 
 
 # erzeugt dreistelliges array, in dem die pins gespeichert sind, die für das aktuelle led set zuständig sind
-def getPins(y):
+def get_pins(y):
     return anodePins[y, 0:]
 
 
 # gibt information an die pins weiter
-def printRegisters(args):
+def print_registers(args):
     # auflösen des parameter tupels
-    # subLeds enthält 16 bit input für schieberegister
-    subLeds = args[0]
-    # subPins enthält die für diese schieberegister zuständigen pins
-    subPins = args[1]
+    # sub_leds enthält 16 bit input für schieberegister
+    sub_leds = args[0]
+    # sub_pins enthält die für diese schieberegister zuständigen pins
+    sub_pins = args[1]
 
-    for x in subLeds:
+    for x in sub_leds:
         # serieller input über den ser pin
-        IO.output(subPins[0], x)
+        IO.output(sub_pins[0], x)
         time.sleep(delay)
 
         # sck bit down flanke. schaltet bits weiter (bit shift des registers)
-        IO.output(subPins[1], 1)
+        IO.output(sub_pins[1], 1)
         time.sleep(delay)
-        IO.output(subPins[1], 0)
+        IO.output(sub_pins[1], 0)
         time.sleep(delay)
 
     # rck bit down flanke. gibt 16 bit information aus
-    IO.output(subPins[2], 1)
+    IO.output(sub_pins[2], 1)
     time.sleep(delay)
-    IO.output(subPins[2], 0)
+    IO.output(sub_pins[2], 0)
     time.sleep(delay)
 
 
 # 04: PROGRAMMVERHALTEN
 
 # setup methode der IO pins
-ledSetup()
+led_setup()
 
 for x in range(kathodePins.size):
     # setzt pin welcher den npn transistor des aktuellen layers steuert auf 1
@@ -111,16 +109,16 @@ for x in range(kathodePins.size):
 
     # THREADING DER VIER SCHIEBEREGISTERPAARE
     # benutzt die Pool Klasse der multiprocessing.dummy Library
-    # pool.map ruft die printRegisters methode mit einem iterator als parameter auf
+    # pool.map ruft die print_registers methode mit einem iterator als parameter auf
     # der iterator ist in dem fall [0,1,2,3], also gibt es insgesamt vier funktionsaufrufe
-    # der parameter für printRegisters ist das Tupel aus getSet und getPins
+    # der parameter für print_registers ist das Tupel aus get_set und get_pins
     # tupel deswegen, weil die map funktion nur über einen Parameter mappen kann
-    # getSet liefert das 16 stellige array als input für die schieberegister
-    # getPins liefert die für das jeweilie Set zuständigen IO Pins
-    # das tupel wird in der printRegisters methode wieder aufgelöst
+    # get_set liefert das 16 stellige array als input für die schieberegister
+    # get_pins liefert die für das jeweilie Set zuständigen IO Pins
+    # das tupel wird in der print_registers methode wieder aufgelöst
 
     with Pool(4) as pool:
-        pool.map(printRegisters, [(getSet(x, y), getPins(y)) for y in [0, 1, 2, 3]])
+        pool.map(print_registers, [(get_set(x, y), get_pins(y)) for y in [0, 1, 2, 3]])
 
     # setzt layer pin wieder auf 0
     IO.output(kathodePins[x], 0)
