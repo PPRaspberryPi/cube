0b60d6f597e7bf24fa104bb276fb5d67a7e66ebb
Kappa Carrageenan <proneo25@gmail.com>
2018-12-12 00:17:05 +0100

    renamed apiTestSimulation to vCubeAPI and removed unnecessary code

---
+++ vCubeAPI.py
@@ -0,0 +1,24 @@
+import main as cube
+
+
+def led_on(*target_leds):
+    """
+    Schaltet beliebige Menge an LED's an
+    :param target_leds: [<layer>, <Zeile im Layer>, <LED in der Zeile>]
+    :return: none
+    """
+    for x in target_leds:
+        cube.main().cubes[(x[0] + 1) * (x[1] + 1) * (x[2] + 1)].setOn()
+
+
+def led_off(*target_leds):
+    """
+    Schaltet beliebige Menge an LED's aus
+    :param target_leds: [<layer>, <Zeile im Layer>, <LED in der Zeile>]
+    :return: none
+    """
+    for x in target_leds:
+        cube.main().cubes[(x[0] + 1) * (x[1] + 1) * (x[2] + 1)].setOff()
+
+
+cube.main()
