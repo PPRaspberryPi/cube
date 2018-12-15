3af8f5e1584f30d986179d545ccc7e4a2624224a
Kappa Carrageenan <proneo25@gmail.com>
2018-12-15 16:47:26 +0100

    dafaq is goin on

--- vcubetest.py
+++ vcubetest.py
@@ -1,11 +1,18 @@
 import threading
 import vCubeAPI as api
+import time
 
 
 class MyThread(threading.Thread):
     def __init__(self):
         super().__init__()
+
+    def run(self):
+        i = 0
         while True:
+            time.sleep(2)
+            api.led_on([i % 7, i % 7, i % 7])
+            i += 1
             eingabe = input("Nummer[x,y,z]: ")
             api.led_on(eingabe)
 
