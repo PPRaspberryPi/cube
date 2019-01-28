ed63211e78cdea9ec6d49382fc51826c14e743a4
carrageen <proneo25@gmail.com>
2019-01-29 00:13:24 +0100

    Zwei Varianten für die Controllerkacke, mit pool und mit asyncio

---
+++ controllertest.py
@@ -0,0 +1,14 @@
+import asyncio, evdev
+
+pad1 = evdev.InputDevice('/dev/input/event17')
+pad2 = evdev.InputDevice('/dev/input/event18')
+
+async def print_events(device):
+    async for event in device.async_read_loop():
+        print(device.path, evdev.categorize(event), sep=': ')
+
+for device in pad1, pad2:
+    asyncio.ensure_future(print_events(device))
+
+loop = asyncio.get_event_loop()
+loop.run_forever()
--- test_keyMapping.py
+++ test_keyMapping.py
@@ -1,13 +1,21 @@
 # import evdev
 from evdev import InputDevice, categorize
+from multiprocessing import Pool
 
-# creates object 'gamepad' to store the data
-# you can call it whatever you like
-gamepad = InputDevice('/dev/input/event3')
 
-# prints out device info at start
-print(gamepad)
+def main(device):
+    # creates object 'gamepad' to store the data
+    # you can call it whatever you like
+    gamepad = InputDevice('/dev/input/event' + str(device))
 
-# evdev takes care of polling the controller in a loop
-for event in gamepad.read_loop():
-    print(categorize(event))
+    # prints out device info at start
+    print(gamepad)
+
+    # evdev takes care of polling the controller in a loop
+
+            
+    for event in gamepad.read_loop():
+        print("Pad",device,">>>", categorize(event))
+
+p = Pool(2)
+p.map(main,[17,18])
