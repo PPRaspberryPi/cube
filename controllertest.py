37e9a0df7f421d6c230cfe988edc38225d8b0286
carrageen <proneo25@gmail.com>
2019-01-29 00:25:17 +0100

    updated prints

--- controllertest.py
+++ controllertest.py
@@ -5,7 +5,7 @@ pad2 = evdev.InputDevice('/dev/input/event18')
 
 async def print_events(device):
     async for event in device.async_read_loop():
-        print(device.path, evdev.categorize(event), sep=': ')
+        print(device.path, evdev.categorize(event), sep=' >>> ')
 
 for device in pad1, pad2:
     asyncio.ensure_future(print_events(device))
