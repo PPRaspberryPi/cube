dcbce1355c522c7e23bdaa734616eb35744cbf76
Leon <leon.koth97@gmail.com>
2019-02-04 22:09:17 +0100

    dingens

---
+++ multiProcessingTest2.py
@@ -0,0 +1,30 @@
+import multiprocessing
+import time
+
+cube_size = 8
+buffer_leds = [x % 2 for x in range(cube_size)]
+leds = multiprocessing.Array('i', cube_size)
+
+
+def display(leds):
+    """for i, num in enumerate(buffer_leds):
+        leds[i] = num"""
+    leds[:] = buffer_leds
+    print("changed array")
+
+
+def print_registers(leds):
+    while True:
+        print(leds[:])
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    p = multiprocessing.Process(target=print_registers, args=(leds,))
+    p.start()
+    time.sleep(2)
+    display(leds)
+    time.sleep(2)
+    buffer_leds = [x % 3 for x in range(cube_size)]
+    display(leds)
+    p.join()
