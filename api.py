00eeb3055bd5fec4dae2c902204852672c93c47b
Leon Koth <leon.koth97@gmail.com>
2019-02-06 17:06:38 +0100

    fix

--- api.py
+++ api.py
@@ -339,6 +339,7 @@ def gamepad1(dir):
             print("no gamepad found")
         for eve in events:
             if eve.code == "ABS_Y" and eve.state == 0:
+                print("p1")
                 dir.value = 1
             if eve.code == "ABS_Y" and eve.state == 255:
                 dir.value = 2
@@ -363,6 +364,7 @@ def gamepad2(dir):
         for eve in events:
             if eve.code == "ABS_Y" and eve.state == 0:
                 dir.value = 1
+                print("p2")
             if eve.code == "ABS_Y" and eve.state == 255:
                 dir.value = 2
             if eve.code == "ABS_X" and eve.state == 255:
